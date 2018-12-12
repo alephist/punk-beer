2aa729b2bb0e1f09c38dcf60d3e181e10c02d155
Justin Necesito <justin.necesito@gmail.com>
2018-12-12 15:09:48 +0800

    Added Beer Card component

---
+++ src/components/BeerCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="column is-4">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-128x128">
+          <img :src="item.image_url" :alt="item.name">
+        </figure>
+      </div>
+
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-semibold">{{ item.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BeerCard",
+  props: ["item"]
+};
+</script>
+
+<style scoped>
+.card-image {
+  padding-top: 15px;
+}
+
+.image img {
+  width: auto;
+  height: 100%;
+}
+
+.image,
+img {
+  margin: auto;
+}
+</style>
